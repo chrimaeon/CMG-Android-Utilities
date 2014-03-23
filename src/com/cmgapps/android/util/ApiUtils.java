3eb299692f3a35633a1a5e82bc9b272d987dceb7
Christian Grach <christian.grach@gmx.at>
2014-03-23 20:24:42 +0100

    update ApiUtils - adds IceCream Sandwich

--- src/com/cmgapps/android/util/ApiUtils.java
+++ src/com/cmgapps/android/util/ApiUtils.java
@@ -38,4 +38,9 @@ public class ApiUtils
   {
     return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1;
   }
+  
+  public static boolean hasIceCreamSandwitch()
+  {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+  }
 }
