6bf8f1689441e00708be985694583693d0dcb416
Christian Grach <christian.grach@gmx.at>
2014-07-26 12:17:44 +0200

    change copyright notice at BitmapCache.java

--- src/com/cmgapps/android/util/BitmapCache.java
+++ src/com/cmgapps/android/util/BitmapCache.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Christian Grach
+ * Copyright 2014 Christian Grach
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
